01755b2bbf70c8da82b576353c2adb7531ab61f3
Lisa2008 <zhang.lisa2008@gmail.com>
2020-06-09 15:23:38 +1000

    Reorder List 143

--- medium/reorder-list-143.js
+++ medium/reorder-list-143.js
@@ -1,3 +1,29 @@
+/**
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example 1:
+
+Given 1->2->3->4, reorder it to 1->4->2->3.
+Example 2:
+
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+
+**/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
 var reorderList = function(head) {
     if(typeof head === 'undefined' || !head) return head;
   if(typeof head.next === 'undefined' || !head.next) return head;
@@ -9,26 +35,20 @@ var reorderList = function(head) {
   while(node){
     array.push(node);
     node = node.next;
+    array[array.length -1].next = null;
     index++;
   }
   
    let len = array.length - 1;
   
-  for(index = 0; index <= Math.ceil(array.length / 2); index++){
+  for(index = 0; index < array.length; index++){
     if((index % 2) === 0){
-      array[index/2].next = array[len - (index/2)];  
+      if(index === len) array[index/2].next = null;
+      else array[index/2].next = array[len - (index/2)];  
     }else{
-      array[len - Math.ceil(index/2) + 1].next = array[Math.ceil(index/2)];
+      if(index === len) array[len - Math.ceil(index/2) + 1].next = null;
+      else array[len - Math.ceil(index/2) + 1].next = array[Math.ceil(index/2)];
     }
-  }
-  
-  
-  if((array.length % 2) === 1){
-    array[len / 2].next = null;
-    //array[array.length - len / 2].next = array[len/2];
-    array[len / 2 + 1].next = array[len / 2];
-  } else {
-    array[array.length / 2].next = null;
   }
   
   return head;
