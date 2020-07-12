c0287ee945ebc40e0f0c1f968b662e4f52ed86f8
Lisa2008 <zhang.lisa2008@gmail.com>
2020-07-12 16:32:23 +1000

    212 Word Search II

--- hard/212-word-search-II.js
+++ hard/212-word-search-II.js
@@ -55,14 +55,82 @@ Trie.prototype.insert = function(word){
  */
 var findWords = function(board, words) {
   let trie = new Trie();
-  
+  let ret = [];
   
   for( let word of words){
     trie.insert(word);  
   }
   
+  ret = find(board, trie, ret);
   
+  return ret;
 };
+
+function find(board, trie, ret){
+  let root = trie.root;
+  
+  for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
+      
+      find1(board, i, j, root, [], ret);
+    }
+  }
+  
+  return ret;
+}
+
+function find1(board, x, y, node, visited, ret){
+ 
+  let lenx = board.length;
+  let leny = board[x].length;
+  
+  /*console.log(`x: ${x} y: ${y}`);
+  console.log(node);
+  console.log(visited);
+  console.log(ret);*/
+  
+  
+  for(let c of Object.keys(node.children)){
+    if(c === board[x][y]){
+      let visitedcur = [...visited];
+      visitedcur.push({[c]: [x, y]});
+      if(node.children[c].end) {
+        let temps = getWord(visitedcur);
+        if(ret.indexOf(temps) === -1) {
+          ret.push(temps);
+        }
+        if(Object.keys(node.children[c].children) === 0) continue;
+      }
+      
+      let findw = false;
+      
+      if((x + 1) < lenx && !isVisited(visited, [x + 1, y])) findw = find1(board, x + 1, y, node.children[c], visitedcur, ret);
+      if((x - 1) >= 0 && !isVisited(visited, [x - 1, y]) && !findw) findw = find1(board, x - 1, y, node.children[c], visitedcur, ret);
+      if((y + 1) < leny && !isVisited(visited, [x, y + 1]) && !findw) findw = find1(board, x, y + 1, node.children[c], visitedcur, ret);
+      if((y - 1) >= 0 && !isVisited(visited, [x, y - 1]) && !findw) findw = find1(board, x, y - 1, node.children[c], visitedcur, ret);
+    }
+  }
+  
+  //console.log('\r\n');
+}
+
+function getWord(v){
+  return v.reduce((acc,cur) => {
+    for(let kk of Object.keys(cur)){
+      acc = acc + kk;
+    }
+    return acc;
+  }, '');
+}
+
+function isVisited(visited, cor){
+  for(let obj of visited){
+    for(let val of Object.values(obj)){
+      if(val[0] === cor[0] && val[1] === cor[1]) return true;
+    }
+  }
+  return false;
+}
 
 let board = [
   ['o','a','a','n'],
@@ -73,6 +141,24 @@ let board = [
 
 let ws =  ["oath","pea","eat","rain"];
 
-console.log(findWords([], ws));
+//console.log(findWords(board, ws));
+
+let board1 = [['a', 'a']];
+let ws1 = ['a'];
+
+//console.log(findWords(board1, ws1));
+
+let board2 = [['a', 'b'],
+              ['c', 'd']];
+
+let ws2 = ["ab","cb","ad","bd","ac","ca","da","bc","db","adcb","dabc","abb","acb"];
+//console.log(findWords(board2, ws2));
+
+let board3 = [['a', 'b'],
+              ['a', 'a']];
+let ws3 = ["aba","baa","bab","aaab","aaa","aaaa","aaba"]
+              
+console.log(findWords(board3, ws3));
+
 
 
