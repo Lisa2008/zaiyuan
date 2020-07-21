b78ffa40b82e55086c53c5500179f2c52adcce60
Lisa2008 <zhang.lisa2008@gmail.com>
2020-07-21 16:36:56 +1000

    271 Encode and Decode Strings

---
+++ medium/271-encode-and-decode-strings.js
@@ -0,0 +1,66 @@
+/**
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+Machine 1 (sender) has the function:
+
+string encode(vector strs) {
+  // ... your code
+  return encoded_string;
+}
+Machine 2 (receiver) has the function:
+vector decode(string s) {
+  //... your code
+  return strs;
+}
+So Machine 1 does:
+
+string encoded_string = encode(strs);
+and Machine 2 does:
+
+vector strs2 = decode(encoded_string);
+strs2 in Machine 2 should be the same as strs in Machine 1.
+
+Implement the encode and decode methods.
+
+Note:
+The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+*/
+
+function encode(strs){
+  if(!strs || strs.length === 0) return '';
+  
+  let ret = '';
+  for(let s of strs){
+    ret = ret + s.length + '#' + s;
+  }
+  
+  return ret;
+}
+
+function decode(str){
+  if(!str || str.length === 0) return [];
+  
+  let destr = [];
+  
+  let len;
+  let index;
+  
+  while(str.length > 0){
+    index = str.indexOf('#');
+    if(index === -1) break;
+
+    len = parseInt(str.slice(0,index));
+    str = str.substring(index + 1);
+    destr.push(str.substr(0, len));
+    str = str.substring(len);
+  }
+  
+  
+  return destr;
+}
+
+//console.log(decode(encode(["lint","code","love","you"])));
+console.log(decode(encode(["we", "say", ":", "yes"])));
+
