520e0b63142da77014a861a12db794a2e38d48ef
Lisa2008 <zhang.lisa2008@gmail.com>
2020-08-05 18:53:04 +1000

    HackerRank Greedy Algorithms advanced Reverse Shuffle Merge

--- other/hackerrank-greedy-algorithms-advanced-reverse-shuffle-merge.js
+++ other/hackerrank-greedy-algorithms-advanced-reverse-shuffle-merge.js
@@ -8,79 +8,78 @@ function reverseShuffleMerge(s) {
     count = map.get(c) || 0;
     map.set(c, count + 1);
   });
-  
+  //console.log(map);
   s = reverseString(s);
   //console.log(s);
   
-  let ret = [];
-  let suposLen = len/2;
-  
-  let newMap = new Map();
   let c;
-  let tempArr;
-  let index;
+  let suposLen = len / 2;
+  let ret = [];
+  
+  let mapUsable = new Map(map);
+  let mapCur = new Map();
+  let usableCount;
+  let instrCount;
+  let j;
+  //len = 45;
   for(let i = 0; i < len; i++){
     c = s.charAt(i);
-    
-    if(ret.length < suposLen){
-      count = newMap.get(c) || 0;
-      if(count < (map.get(c) / 2)){
-        ret.push(c);
-        newMap.set(c, count + 1);
-      }else{
-        ret = replaceC1(newMap.get(c), ret, c);
-      }
-    }else{
-      ret = replaceC1(newMap.get(c), ret, c);
+    usableCount = mapUsable.get(c);
+    instrCount = mapCur.get(c) || 0;
+    if(instrCount === (map.get(c) / 2)){
+      mapUsable.set(c, usableCount - 1);
+      continue;
     }
-    //console.log(ret.join(''));
+    
+    if(ret.length === 0) {
+      ret.push(c);
+      mapCur.set(c, 1);
+    }
+    else{
+      j = ret.length -1;
+      while(j >= 0){
+        if((mapUsable.get(ret[j]) + mapCur.get(ret[j])) <= (map.get(ret[j]) / 2)) break;
+        if(c < ret[j]){
+          removeElement(ret, j, mapCur);                    
+        }
+        j--;
+      }
+      if(ret.length < len / 2 && (instrCount < (map.get(c) / 2)) ){
+        ret.push(c);
+        mapCur.set(c, instrCount + 1);
+      }
+      //console.log(ret);
+
+    }
+    mapUsable.set(c, usableCount - 1);
   }
-  
   return ret.join('');
-   
 }
 
-function replaceC1(countC, minArr, c){
-  let index = 1;
-  let tempArr;
-  
-  while(index <= countC){
-    tempArr = [...minArr];
-    tempArr = replaceC(tempArr, c, index);
-    if(tempArr < minArr) {
-      return tempArr;
-    }else{
-      index++;
-    }
+
+function removeElement(arr, index, map){
+  let temp;
+  for(let i = arr.length - 1; i >= index; i--){
+    temp = map.get(arr[i]);
+    map.set(arr[i], temp - 1);
+    arr.pop();
   }
-  return minArr;
 }
+
+
 
 function reverseString(s){
   return [...s].reverse().join('');
 }
 
-function replaceC(arr, c, count){
-  let len = arr.length;
-  let i;
-  let index = 1;
-  for(i = 0; i < len; i++){
-    if(arr[i] === c){
-      if(index === count) break;
-      else index++;
-    }
-  }
-  arr.splice(i,1);
-  arr.push(c);
-  
-  return arr;
-}
 
 //console.log(reverseShuffleMerge('abcabc')); // acb
 //console.log(reverseShuffleMerge('eggegg')); // egg
 //console.log(reverseShuffleMerge('abcdefgabcdefg')); //agfedcb
-//console.log(reverseShuffleMerge('bdabaceadaedaaaeaecdeadababdbeaeeacacaba')); //aaaaaabaaceededecbdb
+console.log(reverseShuffleMerge('bdabaceadaedaaaeaecdeadababdbeaeeacacaba')); //aaaaaabaaceededecbdb
 
 let str1 = 'djjcddjggbiigjhfghehhbgdigjicafgjcehhfgifadihiajgciagicdahcbajjbhifjiaajigdgdfhdiijjgaiejgegbbiigida';
 
-console.log(reverseShuffleMerge(str1)); //aaaaabccigicgjihidfiejfijgidgbhhehgfhjgiibggjddjjd
+//console.log(reverseShuffleMerge(str1)); //aaaaabccigicgjihidfiejfijgidgbhhehgfhjgiibggjddjjd
+
+  
